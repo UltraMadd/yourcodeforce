7f053421aec9be377827aa62e9ad73458cfea169
UltraMadd <mad@ultra.Dlink>
2023-07-28 16:18:17 +0700

    format code

--- setup.py
+++ setup.py
@@ -2,17 +2,15 @@ from setuptools import setup
 
 
 setup(
-    name = "ycf",
-    version = "0.0.1",
+    name="ycf",
+    version="0.0.1",
     author="UMadd",
-    description = "Script that helps solving problems",
-    packages = ["ycf"],
-    package_data={'': ["euler.txt"]},
+    description="Script that helps solving problems",
+    packages=["ycf"],
+    package_data={"": ["euler.txt"]},
     entry_points={
         "console_scripts": [
             "ycf = ycf.main:main",
-            ]
-        },
-    )
-
-
+        ]
+    },
+)
